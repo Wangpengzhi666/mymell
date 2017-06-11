029ae3acbdb7d293355827ade7a6769647a9aae2
Wangpengzhi666 <983885385@qq.com>
2017-06-11 20:21:00 +0800

    登录页面做了一半

---
+++ app/src/main/java/com/wpzmall/mymall/view/activity/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.wpzmall.mymall.view.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+import com.wpzmall.mymall.R;
+
+/**
+ * @类作用:
+ * @author: 王鹏智
+ * @Date: 2017/6/11  17:43
+ * <p>
+ * 思路：
+ */
+
+
+public class LoginActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
